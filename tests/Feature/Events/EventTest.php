130dfd4a0c758f347b8c6a48f50e5f44567c8574
James Gudhlanga <jimmyneds@gmail.com>
2019-04-11 13:55:45 +0200

    added event list test

--- tests/Feature/Events/EventTest.php
+++ tests/Feature/Events/EventTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Events;
 
+use App\Modules\Events\Event;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,11 +11,21 @@ class EventTest extends TestCase
 {
    use RefreshDatabase;
 
-   /**
+   protected $event;
+
+   public function setUp(): void
+   {
+       parent::setUp();
+       $this->event = factory(Event::class)->create();
+   }
+    /**
     * @test
     */
-   public function a_user_can_see_list_of_events()
+   public function a_user_can_see_list_of_events(): void
    {
-
+        $this->get(route('events'))
+            ->assertStatus(200)
+            ->assertSeeText($this->event->title)
+            ->assertSeeText($this->event->description);
    }
 }
