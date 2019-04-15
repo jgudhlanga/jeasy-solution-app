bc4ac5a6aa209c3b92093b832a08a0fa4ed2cbdb
James Gudhlanga <jimmyneds@gmail.com>
2019-04-15 08:31:34 +0200

    added auth

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
 });
 
 Route::resource('events', 'Events\EventsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
---
+++ tests/Feature/Auth/AuthTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function that_a_user_can_see_log_in_page() {
+        $this->get(route('login'))
+            ->assertViewIs('auth.login')
+            ->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function that_a_user_can_log_in() {
+        $this->post('login', $this->user->toArray())
+            ->assertRedirect('/');
+    }
+}
