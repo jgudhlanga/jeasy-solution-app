c16e3ff051fca217769abb9c3926aa3ae9303a5c
James Gudhlanga <jimmyneds@gmail.com>
2019-04-18 13:12:32 +0200

    travis fixes

--- app/Utilities/Utility.php
+++ app/Utilities/Utility.php
@@ -1,6 +1,8 @@
 <?php
-if(!function_exists('limit_words')) {
-    function limit_words($string, $wordCount = 100, $end = '...') {
+if (!function_exists('limit_words'))
+{
+    function limit_words($string, $wordCount = 100, $end = '...')
+    {
         return \Illuminate\Support\Str::words($string, $wordCount, $end);
     }
 }
